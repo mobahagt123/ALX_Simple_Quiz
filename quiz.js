a8c5c46921331e4cadf3126b43f2e7b84ffc945c
Mohamed Bahgat <52916078+mobahagt123@users.noreply.github.com>
2024-10-07 17:43:04 +0300

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -11,5 +11,5 @@ function checkAnswer(){
     }
 }
 
-const submit = document.querySelector('#submit-answer')
+const submit = document.getElementById('submit-answer')
 submit.addEventListener('click',checkAnswer)
